40a1499b01936a01b02893404b6b8237846f7627
blueeyesnake <cullen.j.phillips@gmail.com>
2022-10-01 16:40:39 -0500

    Lists

---
+++ money-management-hackathon/src/pages/blogs.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Blogs = () => {
+return (
+	<ul>
+		<li>Pack your lunch</li>
+  		<li>Bike to destinations when possible</li>
+  		<li>Coupons</li>
+		<li>Go to events with free food (hackathon)</li>
+		<li>Don't eat out</li>
+	</ul>
+);
+};
+
+export default Blogs;
